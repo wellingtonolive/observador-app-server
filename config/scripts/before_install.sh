c6b89d73ab5e11babe4fe0cbcd3ed977249b6578
wellingtonolive <wellingtoncarvalho11@gmail.com>
2021-02-28 19:39:22 -0300

    Teste

--- config/scripts/before_install.sh
+++ config/scripts/before_install.sh
@@ -6,19 +6,3 @@ echo "Cd realizado com Sucesso"
 sudo rm -rf /home/ubuntu/*
 echo "Limpando repositorio"
 
-
-curl -fsSL https://deb.nodesource.com/setup_15.x | sudo -E bash -
-
-echo "Curl realizado com Sucesso"
-
-sudo apt-get install -y nodejs
-
-echo "Install NodeJS com Sucesso"
-
-sudo npm install
-
-echo "npm install realizado com sucesso"
-
-sudo npm install -g npm@7.5.4
-
-echo "instalando biblioteca especifica"
