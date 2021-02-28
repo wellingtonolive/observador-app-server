02d4ea3e5ef8a476bf49420e50cb71bf97e23016
wellingtonolive <wellingtoncarvalho11@gmail.com>
2021-02-28 19:24:36 -0300

    Scripts

--- config/scripts/app_stop.sh
+++ config/scripts/app_stop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd /opt/app/observador-app-server/
-echo 'CD Realizado com Sucesso'
+cd /home/ubuntu
+echo "CD Realizado com Sucesso"
 
 fuser -k 1234/tcp
-echo 'Parou processo na porta 1234'
+echo "Parou processo na porta 1234"
 
-echo 'Executou'
+echo "Executou"
