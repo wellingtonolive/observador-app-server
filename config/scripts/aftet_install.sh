03ad8ab10c9cfa074ff61ea152d4dd98ff3a4482
wellingtonolive <wellingtoncarvalho11@gmail.com>
2021-03-01 01:05:12 -0300

    Ajuste

--- config/scripts/aftet_install.sh
+++ config/scripts/aftet_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Limpando Pasta do Antigo Server"
 sudo rm -R /opt/app/
+echo "Criando Novo Diretorio"
+sudo mkdir /opt/app/observador
 echo "Colocando novos Arquivos"
-sudo cp -r /home/ubuntu /opt/app/observador
+sudo cp -r /home/ubuntu/api /opt/app/observador
