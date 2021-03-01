07c2d9e7fe0627043b06ff99ea7459717e66ac58
wellingtonolive <wellingtoncarvalho11@gmail.com>
2021-03-01 10:13:52 -0300

    Ajuste para stopar servidor

--- config/scripts/app_start.sh
+++ config/scripts/app_start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+echo "Abrindo pasta da API"
 cd /opt/app/observador
+echo "Parando servidor"
+fuser -k 1234/tcp
+echo "Iciando "
 npm start &
--- config/scripts/app_stop.sh
+++ config/scripts/app_stop.sh
@@ -3,3 +3,4 @@ cd /opt/app/observador
 echo "CD Realizado com Sucesso"
 fuser -k 1234/tcp
 echo "Parou processo na porta 1234"
+npm start &
