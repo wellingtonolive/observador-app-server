95800041db7ba42f9cda3816bfd85182cec92674
wellingtonolive <wellingtoncarvalho11@gmail.com>
2021-03-01 01:15:26 -0300

    Diretorios

--- config/scripts/aftet_install.sh
+++ config/scripts/aftet_install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 echo "Limpando Pasta do Antigo Server"
 sudo rm -R /opt/app/
-echo "Criando Novo Diretorio"
-sudo mkdir /opt/app/observador
+echo "Entrando na Pasta OPT"
+cd /opt
+echo "Criando Novo Diretorio APP"
+sudo mkdir app
+echo "Entrando na Pasta APP"
+cd /opt/app/
+echo "Criando Pasta para API Observador"
+sudo mkdir observador
 echo "Colocando novos Arquivos"
 sudo cp -r /home/ubuntu/api /opt/app/observador
