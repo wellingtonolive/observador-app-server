e24521a892509a789f4765b1156631307f53e3b3
fellype <fellype.ads@gmail.com>
2021-04-07 11:10:30 -0300

    tentativa de salvar imagem

--- routes/Arquivos.js
+++ routes/Arquivos.js
@@ -1,11 +1,14 @@
-const router = require('express').Router();
-const multer = require('multer');
-const multerConfig = require('../config/multerConfig');
-const arquivoController = require('../controllers/ArquivoController')
+const router = require("express").Router();
+const multer = require("multer");
+const multerConfig = require("../config/multerConfig");
+const arquivoController = require("../controllers/ArquivoController");
 
-router.get('/arquivos', arquivoController.consultar);
-router.post('/arquivos', multer(multerConfig).single('file'),arquivoController.salvar)
-
+router.get("/arquivos", arquivoController.consultar);
+router.post(
+  "/arquivos",
+  multer(multerConfig).single("file"),
+  arquivoController.salvar
+);
+router.delete("/arquivos:id", arquivoController.apagar);
 
 module.exports = router;
-
