1ac69273eefbf552f3cf7c0df6113894bffd71ab
fellype <fellype.ads@gmail.com>
2021-04-15 16:35:00 -0300

    before

--- config/scripts/before_install.sh
+++ config/scripts/before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/api
 echo "Cd realizado com Sucesso"
-sudo cp -R /opt/app/observador /home/ubuntu/api/$(date +'%Y-%m-%d-%T')
-echo "BackUp do Fonte Anterior Salvo em: /home/ubuntu/api/"$(date +'%Y-%m-%d-%T')
+#sudo cp -R /opt/app/observador /home/ubuntu/api/$(date +'%Y-%m-%d-%T')
+#echo "BackUp do Fonte Anterior Salvo em: /home/ubuntu/api/"$(date +'%Y-%m-%d-%T')
 
