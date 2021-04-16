20df09dd47d78f0d37362e5d42d943d030109422
wellingtonolive <wellingtoncarvalho11@gmail.com>
2021-04-16 15:55:32 -0300

    Ajustes Deploy

--- config/scripts/aftet_install.sh
+++ config/scripts/aftet_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ubuntu/api/app
+cd /home/ubuntu/api
 echo "CD Realizado"
 npm install
 npm install --save react react-dom react-scripts react-particles-js
--- config/scripts/app_start.sh
+++ config/scripts/app_start.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-cd /home/ubuntu/api/app
+cd /home/ubuntu/api
 echo "Abrindo pasta da API com novo Build"
 sudo rm -Rf /opt/app/observador/
 echo "Limpando Diretório /opt/app/observador"
-sudo cp -r /home/ubuntu/api/app/ /opt/app/observador
+sudo mv /home/ubuntu/api/ /opt/app/observador
 echo "Movendo a Pasta do Novo Build para /opt/app/observador"
-sudo rm -r /home/ubuntu/api/app/*
-echo "Limpando Pasta do Server"
 cd /opt/app/observador
 echo "Acessando Pasta do Servidor"
 pm2 start server.js
