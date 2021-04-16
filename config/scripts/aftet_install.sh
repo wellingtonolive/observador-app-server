053bd4a05225fd01f4b18572bf3485a2f7c6fb5b
wellingtonolive <wellingtoncarvalho11@gmail.com>
2021-04-16 17:13:36 -0300

    Ajustes

--- config/scripts/aftet_install.sh
+++ config/scripts/aftet_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/api
 echo "CD Realizado"
-npm install
-npm install --save react react-dom react-scripts react-particles-js
-npm install pm2 -g
+sudo npm install
+sudo npm install pm2 -g
 echo "Instalações concluídas com sucesso"
