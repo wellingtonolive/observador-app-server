4ca3c86edf0b265519ad9638c8cc37a10a7187c0
wellingtonolive <wellingtoncarvalho11@gmail.com>
2021-04-18 00:42:02 -0300

    Organizando Arquivos da Pipeline

--- config/scripts/aftet_install.sh
+++ config/scripts/aftet_install.sh
@@ -4,3 +4,11 @@ echo "CD Realizado"
 sudo npm install
 sudo npm install pm2 -g
 echo "Instalações concluídas com sucesso"
+cd /home/ubuntu/api
+echo "Abrindo pasta da API com novo Build"
+sudo rm -Rf /opt/app/observador/
+echo "Limpando Diretório /opt/app/observador"
+sudo cp -R /home/ubuntu/api/ /opt/app/observador
+echo "Movendo a Pasta do Novo Build para /opt/app/observador"
+sudo rm -Rf /home/ubuntu/api/
+echo "Limpando Local Onde Recebe Arquivos do Build"
--- config/scripts/app_start.sh
+++ config/scripts/app_start.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu/api
-echo "Abrindo pasta da API com novo Build"
-sudo rm -Rf /opt/app/observador/
-echo "Limpando Diretório /opt/app/observador"
-sudo mv /home/ubuntu/api/ /opt/app/observador
-echo "Movendo a Pasta do Novo Build para /opt/app/observador"
 cd /opt/app/observador
 echo "Acessando Pasta do Servidor"
 pm2 start server.js
