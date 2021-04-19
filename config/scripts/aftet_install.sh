3f7ca8f710c51f1a042acedcc95cec0c7dfa81cd
wellingtonolive <wellingtoncarvalho11@gmail.com>
2021-04-19 01:16:13 -0300

    Ajustes para pegar arquivos ocultos

--- config/scripts/aftet_install.sh
+++ config/scripts/aftet_install.sh
@@ -6,9 +6,9 @@ sudo npm install pm2 -g
 echo "Instalações concluídas com sucesso"
 cd /home/ubuntu/api
 echo "Abrindo pasta da API com novo Build"
-sudo rm -Rf /opt/app/observador/*
+sudo rm -Rf /opt/app/observador/
 echo "Limpando Diretório /opt/app/observador"
-sudo cp -R /home/ubuntu/api/* /opt/app/observador
+sudo cp -R /home/ubuntu/api/ /opt/app/observador
 echo "Movendo a Pasta do Novo Build para /opt/app/observador"
-sudo rm -R /home/ubuntu/api/*
+#sudo rm -R /home/ubuntu/api/*
 echo "Limpando Local Onde Recebe Arquivos do Build"  
