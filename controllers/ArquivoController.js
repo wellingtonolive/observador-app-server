1d10cf50dcc2a49e59d7547e60d5f24dd18a424c
fellype <fellype.ads@gmail.com>
2021-05-03 17:47:47 -0300

    Revert "pdf"
    This reverts commit 302da6ac306723a8edbafee6918f43961700d612.

--- controllers/ArquivoController.js
+++ controllers/ArquivoController.js
@@ -131,13 +131,6 @@ module.exports = {
         },
       };
 
-      const fundaments = {
-        ruim: 0,
-        normal: 1,
-        bom: 2,
-        "acima da média": 3,
-      };
-
       const athlete = {
         gameName: respGame.gameName,
         gameCategory: respGame.category,
@@ -154,20 +147,12 @@ module.exports = {
         birthDate: dateNow(respAthlete?.birthDate, false, true),
         team: respAthlete?.team,
         skillLeg: respAthlete?.skillLeg,
-        shirtNumber: restpAhlete?.shirtNumber,
+        shirtNumber: respAthlete?.shirtNumber,
         age:
           new Date().getFullYear() -
           (respAthlete?.year !== undefined
             ? respAthlete.year
             : new Date().getFullYear()),
-        shortPass: fundaments[restpAhlete?.shortPass || 25],
-        longPass: fundaments[restpAhlete?.longPass || 25],
-        header: fundaments[restpAhlete?.header || 25],
-        position: fundaments[restpAhlete?.position || 25],
-        velocity: fundaments[restpAhlete?.velocity || 25],
-        reactionPower: fundaments[restpAhlete?.reactionPower || 25],
-        mobility: fundaments[restpAhlete?.mobility || 25],
-        finalization: fundaments[restpAhlete?.finalization || 25],
       };
 
       const document = {
